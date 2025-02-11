7c86d5d8cad988f235c9da02234fe51926adb4cc
iiyama LEVEL ∞ <iiyama LEVEL @UEYAMA-PC>
2025-02-11 15:22:26 +0900

    20250211 上山 コミットテスト

---
+++ HelloJava/src/co/jp/gcns/HelloJava.java
@@ -0,0 +1,16 @@
+package co.jp.gcns;
+
+/**
+ * 
+ */
+public class HelloJava {
+	
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello JAVA!!");
+	}
+}
