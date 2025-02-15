3679fb9692f5869e695a36827ca064c9c2a07cc2
iiyama LEVEL ∞ <iiyama LEVEL @UEYAMA-PC>
2025-02-15 10:28:53 +0900

    2025/2/15　上山 コミットのテスト

--- GCNSStudy2025/src/HelloJavaMain.java
+++ GCNSStudy2025/src/HelloJavaMain.java
@@ -1,6 +1,14 @@
-
+/**
+ * 
+ */
 public class HelloJavaMain {
+	
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
+		// システムアウト出力
 		System.out.println("Hello Java!!!");
 	}
 }
